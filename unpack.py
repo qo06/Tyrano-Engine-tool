65f790106891b2aed3740c8da929bb525cc2a590
Rosa <108557334+qo06@users.noreply.github.com>
2025-06-09 23:28:00 +0400

    Source added

---
+++ unpack.py
@@ -0,0 +1,60 @@
+from struct import unpack
+from json import loads
+from pathlib import Path
+from os import makedirs,getcwd
+from os.path import sep,abspath
+import sys
+
+def FolderLoop(Data='',Current='',sf=0) -> list:
+    OutList = []
+    Data = Data['files']
+    for i in Data.items():
+
+        # Check if this is a folder or not.
+        if list(i[1].items())[0][0] == 'files':
+            OutList.append([ Current + sep + i[0] , 0 , 0])
+            OutList += FolderLoop( i[1],\
+                                   Current = Current + sep + i[0] ,\
+                                   sf=sf)
+        else:
+            FileName = i[0]
+            Size     = list(i[1].items())[0][1]
+            Offset   = int(list(i[1].items())[1][1])
+
+            OutList.append([ Current + sep + FileName,\
+                             Size,\
+                             Offset + sf ])
+    return OutList
+
+def extract(File,OutDir=""):
+
+    ExecPath = File.split(".")[0] + sep
+    
+    with open(File,'rb') as fr:
+
+        _,val_2,_,IndexSize = unpack('<IIII',fr.read(16))
+        
+        fr.seek(16,0)
+        
+        Index =  loads( fr.read(IndexSize).decode('utf8') )
+
+        Index = FolderLoop(Index,\
+                           Current='',\
+                           sf=val_2+8)
+        
+        for i in Index:
+            print(i[0])
+        
+            if i[1] == 0 and i[2] == 0:
+                makedirs(ExecPath + i[0],exist_ok=True)
+            else:
+                fr.seek(i[2],0)
+                Data = fr.read(i[1])
+
+                with open(ExecPath + i[0] ,'wb') as fw:
+                    fw.write(Data)
+                    fw.close()
+        fr.close()
+
+if __name__ == '__main__':
+    extract(sys.argv[1])
